a94dd876a1f358fa2164cd398adc6adc38218cd1
Carlos Ramos <cj.ramos@sarria.salesians.cat>
2020-10-21 19:42:28 +0200

    Formulario Prototipado
    Se añade el formulario de "Prototipo" que se encuentra vació y en blanco.

---
+++ PROTOTIPADO.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace timer
+{
+    partial class PROTOTIPADO
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PROTOTIPADO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1069, 541);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "PROTOTIPADO";
+            this.Text = "PROTOTIPADO";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
